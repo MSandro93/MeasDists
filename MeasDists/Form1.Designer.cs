ad71b236b18ca24f2b778c91914dd80cc2504669
Michael Stoffels <michael.stoffels@nerdtronix.de>
2024-01-31 18:17:06 +0100

    init

---
+++ MeasDists/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace MeasDists
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.drawing_surface = new System.Windows.Forms.PictureBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.load_butt = new System.Windows.Forms.Button();
+            this.toggle_grid_butt = new System.Windows.Forms.Button();
+            this.rotate_scrollBar = new System.Windows.Forms.HScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.drawing_surface)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawing_surface
+            // 
+            this.drawing_surface.Location = new System.Drawing.Point(12, 12);
+            this.drawing_surface.Name = "drawing_surface";
+            this.drawing_surface.Size = new System.Drawing.Size(940, 560);
+            this.drawing_surface.TabIndex = 0;
+            this.drawing_surface.TabStop = false;
+            // 
+            // load_butt
+            // 
+            this.load_butt.Location = new System.Drawing.Point(977, 12);
+            this.load_butt.Name = "load_butt";
+            this.load_butt.Size = new System.Drawing.Size(141, 23);
+            this.load_butt.TabIndex = 1;
+            this.load_butt.Text = "&Load from clipbaord";
+            this.load_butt.UseVisualStyleBackColor = true;
+            this.load_butt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toggle_grid_butt
+            // 
+            this.toggle_grid_butt.Enabled = false;
+            this.toggle_grid_butt.Location = new System.Drawing.Point(977, 50);
+            this.toggle_grid_butt.Name = "toggle_grid_butt";
+            this.toggle_grid_butt.Size = new System.Drawing.Size(141, 23);
+            this.toggle_grid_butt.TabIndex = 2;
+            this.toggle_grid_butt.Text = "&Toggle Grid";
+            this.toggle_grid_butt.UseVisualStyleBackColor = true;
+            this.toggle_grid_butt.Click += new System.EventHandler(this.toggle_grid_butt_Click);
+            // 
+            // rotate_scrollBar
+            // 
+            this.rotate_scrollBar.LargeChange = 5;
+            this.rotate_scrollBar.Location = new System.Drawing.Point(977, 105);
+            this.rotate_scrollBar.Maximum = 200;
+            this.rotate_scrollBar.Minimum = -200;
+            this.rotate_scrollBar.Name = "rotate_scrollBar";
+            this.rotate_scrollBar.Size = new System.Drawing.Size(144, 17);
+            this.rotate_scrollBar.TabIndex = 3;
+            this.rotate_scrollBar.TabStop = true;
+            this.rotate_scrollBar.ValueChanged += new System.EventHandler(this.rotate_scrollBar_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(974, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "adjust rotation";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(977, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "&Set reference";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 647);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rotate_scrollBar);
+            this.Controls.Add(this.toggle_grid_butt);
+            this.Controls.Add(this.load_butt);
+            this.Controls.Add(this.drawing_surface);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.drawing_surface)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox drawing_surface;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button load_butt;
+        private System.Windows.Forms.Button toggle_grid_butt;
+        private System.Windows.Forms.HScrollBar rotate_scrollBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MeasDists/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+
+namespace MeasDists
+{
+    public partial class Form1 : Form
+    {
+        Image source_image = null;
+        int sys_state = 0;
+        bool grid_toggle_grid = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsImage())
+            {
+                source_image = Clipboard.GetImage();
+                drawing_surface.Image = (Image)source_image.Clone();
+
+                sys_state = 1;
+                toggle_grid_butt.Enabled = true;
+            }
+
+            else
+            {
+                MessageBox.Show("No image found in clipboard");
+                sys_state = 0;
+                toggle_grid_butt.Enabled = false;
+            }
+        }
+
+        //TODO: maybe fix the grid to be more homogeneous. Not sure about this so far..
+        private void toggle_grid_butt_Click(object sender, EventArgs e)
+        {
+            grid_toggle_grid ^= true;
+
+            drawing_surface.Image.Dispose();
+            drawing_surface.Image = (Image)source_image.Clone();
+
+            apply_roation(drawing_surface.Image, rotate_scrollBar.Value / 10.0f);
+
+            if (grid_toggle_grid)
+            {
+                Pen p_ = new Pen(Color.Red, 1);
+                Graphics g_ = Graphics.FromImage(drawing_surface.Image);
+
+                //draw 10 vertical grid lines
+                int width_grid = (int)(drawing_surface.Width / 10.0f);
+                for (int x_ = width_grid; x_ < drawing_surface.Width; x_ += width_grid)
+                {
+                    g_.DrawLine(p_, x_, 0, x_, drawing_surface.Height);
+                }
+                //
+
+                //draw 10 horizontal grid lines
+                int height_grid = (int)(drawing_surface.Height / 10.0f);
+                for (int y_ = height_grid; y_ < drawing_surface.Height; y_ += height_grid)
+                {
+                    g_.DrawLine(p_, 0, y_,  drawing_surface.Width, y_);
+                }
+                //
+                drawing_surface.Refresh();
+            }
+
+            else
+            {
+                drawing_surface.Refresh();
+                return;
+            }
+        }
+
+        private void rotate_scrollBar_ValueChanged(object sender, EventArgs e)
+        {
+
+            Image img_tmp = source_image;
+            Graphics g_ = Graphics.FromImage(img_tmp);
+
+            apply_roation(img_tmp, rotate_scrollBar.Value / 10.0f);
+
+            // draw grid at rotated image if toggled on.
+            if (grid_toggle_grid)
+            {
+                Pen p_ = new Pen(Color.Red, 1);
+
+                //draw 10 vertical grid lines
+                int width_grid = (int)(drawing_surface.Width / 10.0f);
+                for (int x_ = width_grid; x_ < drawing_surface.Width; x_ += width_grid)
+                {
+                    g_.DrawLine(p_, x_, 0, x_, drawing_surface.Height);
+                }
+                //
+
+                //draw 10 horizontal grid lines
+                int height_grid = (int)(drawing_surface.Height / 10.0f);
+                for (int y_ = height_grid; y_ < drawing_surface.Height; y_ += height_grid)
+                {
+                    g_.DrawLine(p_, 0, y_, drawing_surface.Width, y_);
+                }
+                //
+                drawing_surface.Image = img_tmp;
+                drawing_surface.Refresh();
+            }
+
+            else
+            {
+                drawing_surface.Image = img_tmp;
+                drawing_surface.Refresh();
+                return;
+            }
+            //
+
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            sys_state = 2;
+            rotate_scrollBar.Enabled = false;
+
+            //apply current rotaion so source iamge, thus rotation scroll bar is locked and rotation is fixed now
+            source_image = apply_roation(source_image, rotate_scrollBar.Value / 10.0f);
+            //
+
+        }
+
+        private Image apply_roation(Image img_, float angle_)
+        {
+            //apply current rotaion so source iamge, thus rotation scroll bar is locked and rotation is fixed now
+            Graphics g_ = Graphics.FromImage(img_);
+            g_.TranslateTransform(img_.Width / 2.0f, img_.Height / 2.0f); //set rotation point to center
+            g_.RotateTransform((float)rotate_scrollBar.Value / 10.0f);
+            g_.TranslateTransform((float)-img_.Width / 2.0f, (float)-img_.Height / 2.0f);
+            g_.ResetTransform();
+            g_.DrawImage(img_, 0, 0);  //"save" rotated image.
+            return img_;
+            //
+        }
+
+
+
+    }
+}
